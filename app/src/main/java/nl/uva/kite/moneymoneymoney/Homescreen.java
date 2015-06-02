bd7a99e3e1e1e580318540589f5667a38380f41a
JoerivVuuren <joerivvuuren@hotmail.com>
2015-06-02 13:42:05 +0200

    updated build.gradle

--- app/src/main/java/nl/uva/kite/moneymoneymoney/Homescreen.java
+++ app/src/main/java/nl/uva/kite/moneymoneymoney/Homescreen.java
@@ -9,7 +9,6 @@ import android.view.MenuItem;
 public class Homescreen extends ActionBarActivity {
 
     @Override
-    //teeeeeeeeeeeeeeest navid
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_homescreen);
