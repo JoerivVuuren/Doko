f38eb89a4b64fb986c5ba39b946f3a5a6d04023e
David Smelt <davdms@gmail.com>
2015-06-04 14:04:03 +0200

    Merge branch 'master' of C:\Users\Dav\Desktop\StudioProjects\MoneyMoneyMoney5 with conflicts.

--- app/src/main/java/nl/uva/kite/moneymoneymoney/ProfileScreen.java
+++ app/src/main/java/nl/uva/kite/moneymoneymoney/ProfileScreen.java
@@ -1,12 +1,12 @@
 package nl.uva.kite.moneymoneymoney;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.Activity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.TextView;
 
-public class ProfileScreen extends AppCompatActivity {
+public class ProfileScreen extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
