61b0127d3caea80703d2a9a946e05f01b077c001
ShahrukhZ <s.zaidi_94@hotmail.com>
2015-06-08 17:15:58 +0200

    receive activity

--- app/src/main/java/nl/uva/kite/Doko/MyPushBroadcastReceiver.java
+++ app/src/main/java/nl/uva/kite/Doko/MyPushBroadcastReceiver.java
@@ -1,24 +1,29 @@
 package nl.uva.kite.Doko;
 
+import android.app.Activity;
 import android.app.Notification;
 import android.app.NotificationManager;
 import android.content.Context;
 import android.content.Intent;
 import android.support.v7.app.NotificationCompat;
 import android.util.Log;
+import android.view.View;
+import android.widget.LinearLayout;
 
 import com.parse.ParsePushBroadcastReceiver;
 
 import org.json.JSONException;
 import org.json.JSONObject;
 
-public class MyPushBroadcastReceiver extends ParsePushBroadcastReceiver {
+public class MyPushBroadcastReceiver extends ParsePushBroadcastReceiver{
 
     public static final String PARSE_DATA_KEY = "com.parse.Data";
 
     @Override
     protected void onPushOpen(Context context, Intent intent) {
-        // Implement
+        Intent i = new Intent(context, ReceiveActivity.class);
+        i.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        context.startActivity(i);
     }
 
     @Override
@@ -29,6 +34,15 @@ public class MyPushBroadcastReceiver extends ParsePushBroadcastReceiver {
             String message = data.getString("message");
             String friendName = data.getString("friendName");
             Log.e("", "just received a friend request with message: " + message + " and friendName: " + friendName);
+            ReceiveActivity instance = ReceiveActivity.instance();
+            if(instance != null){
+                instance.setTextView(friendName);
+            }
+            else{
+                Intent i = new Intent(context, ReceiveActivity.class);
+                i.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                context.startActivity(i);
+            }
         } catch(JSONException e) {
             Log.e("", "JSONERROR");
         }
---
+++ app/src/main/java/nl/uva/kite/Doko/ReceiveActivity.java
@@ -0,0 +1,39 @@
+package nl.uva.kite.Doko;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+/**
+ * Created by Shahrukh on 8-6-2015.
+ */
+public class ReceiveActivity extends Activity{
+    private static ReceiveActivity instance;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState){
+        instance = this;
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.requests);
+    }
+
+    public static ReceiveActivity instance() {
+        return instance;
+    }
+
+    public void setTextView(String friendName){
+        LinearLayout insertPoint = (LinearLayout) findViewById(R.id.myLayout);
+        View templateView = getLayoutInflater().inflate(R.layout.template, insertPoint, false);
+
+        TextView name = (TextView) templateView.findViewById(R.id.textView1);
+        name.setText(friendName);
+        insertPoint.addView(name);
+    }
+
+    @Override
+    public void onStop(){
+        instance = null;
+    }
+}
