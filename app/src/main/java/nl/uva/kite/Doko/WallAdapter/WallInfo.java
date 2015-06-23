f4063d7c062a27f5b3460b6beaa436842d1ef347
whenislunch <piraat@gmail.com>
2015-06-23 13:59:27 +0200

    wall adapter and all that stuff ready, all we need now is a database hook

---
+++ app/src/main/java/nl/uva/kite/Doko/WallAdapter/WallInfo.java
@@ -0,0 +1,11 @@
+package nl.uva.kite.Doko.WallAdapter;
+
+/**
+ * Created by whenislunch on 22-6-15.
+ */
+public class WallInfo {
+    public String vPosterName;
+    public String vMessage;
+    public int type;
+    public double amount;
+}
